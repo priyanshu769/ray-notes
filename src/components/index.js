4a4773339bc25cad3997a344bd59e67cdc4b16d1
Priyanshu <prynsu@yahoo.com>
2021-07-30 00:30:37 +0530

    Fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { TakeNote } from './TakeNotes/TakeNote'
+export { TakeNote } from './TakeNote/TakeNote'
 export { Note } from './Note/Note'
 export { SeeMore } from './SeeMore/SeeMore'
 export { Sidebar } from './Sidebar/Sidebar'
