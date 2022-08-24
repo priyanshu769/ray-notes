3f092ee5bc118954ab2e992ce2338dc5f4a9bdd4
Priyanshu <prynsu@yahoo.com>
2022-08-25 01:48:56 +0530

    Refactor: Updated Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,70 +1,50 @@
-import { NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router'
 import { useApp } from '../../contexts/AppContext'
 import './Sidebar.css'
 
 export const Sidebar = () => {
-  const { state, dispatch } = useApp()
-  const logoutHandler = () => {
-    localStorage.removeItem('token')
-    dispatch({ type: 'SET_LOGGEDIN_TOKEN', payload: null })
-    dispatch({
-      type: 'SET_PROFILE',
-      payload: {
-        userId: 'u123',
-        name: 'James Bucky Barns',
-      },
-    })
-    dispatch({
-      type: 'SET_NOTES',
-      payload: [
-        {
-          _id: 'n123',
-          title: 'Note Example',
-          content: 'Your note will look like this. Go create your first Note',
-          color: '#ffffff',
-          pinned: false,
-          user: {
-            userId: 'u123',
-          },
-        },
-      ],
-    })
-  }
-  return (
-    <div
-      style={{ display: state.loggedInToken ? 'block' : 'none' }}
-      className="sidebar"
-    >
-      <h3 className="profileHeadline">Profile</h3>
-      <p className="profileName">{`${state.profile.name.split(' ')[0]}`}</p>
-      <h3>Navigate</h3>
-      <ul className="navLinks">
-        <li className="navLinkLi">
-          <NavLink activeClassName="activeNavLink" className="navLink" to="/">
-            Home
-          </NavLink>
-        </li>
-        <li className="navLinkLi">
-          <NavLink
-            activeClassName="activeNavLink"
-            className="navLink"
-            to="/create-note"
-          >
-            Create
-          </NavLink>
-        </li>
-      </ul>
-      <h3>Your Notes</h3>
-      <ul className="noteTitles">
-        {state.notes.map((note) => {
-          return (
-            <li className="noteTitleLi">
-              <button className="noteTitleBtn">{note.title}</button>
-            </li>
-          )
-        })}
-      </ul>
-      <button onClick={() => logoutHandler()}>Logout</button>
-    </div>
-  )
+    const { state, dispatch } = useApp()
+    const navigate = useNavigate()
+    const logoutHandler = () => {
+        localStorage.removeItem('token')
+        dispatch({ type: 'SET_LOGGEDIN_TOKEN', payload: null })
+        dispatch({
+            type: 'SET_PROFILE',
+            payload: {
+                userId: 'u123',
+                name: 'James Bucky Barns',
+            },
+        })
+        dispatch({
+            type: 'SET_NOTES',
+            payload: [
+                {
+                    _id: 'n123',
+                    title: 'Note Example',
+                    content: 'Your note will look like this. Go create your first Note',
+                    color: '#ffffff',
+                    pinned: false,
+                    user: {
+                        userId: 'u123',
+                    },
+                },
+            ],
+        })
+    }
+    return (
+        <div style={{ display: state.loggedInToken ? 'block' : 'none' }}>
+            <p className="profileName">Hi, {`${state.profile.name.split(' ')[0]}`}!</p>
+            <button className='navBtn' onClick={() => navigate('/')}>Home</button>
+            <br />
+            <button className='navBtn' onClick={() => navigate('/create-note')}>Create Note</button>
+            <h4>Notes</h4>
+            {state.notes.map(note => {
+                return (<div>
+                    <button className="noteTitleBtn" onClick={() => navigate(`/note/${note._id}`)} >{note.title}</button>
+                </div>)
+            })}
+            <br />
+            <button className='noteBtn' onClick={logoutHandler}>Logout</button>
+        </div>
+    )
 }
